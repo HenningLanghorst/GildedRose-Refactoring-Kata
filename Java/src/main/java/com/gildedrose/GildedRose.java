02390f0db2fd71c55cf1429aecda07ddf7c5436a
Henning Langhorst <henning.langhorst@gmail.com>
2023-05-20 17:25:02 +0200

    restructure code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,35 +9,23 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            int adjustment = switch (item.name) {
-                case "Aged Brie" -> ageBrieQualityChange(item);
-                case "Backstage passes to a TAFKAL80ETC concert" -> backstagePassQualityChange(item);
-                case "Sulfuras, Hand of Ragnaros" -> 0;
-                default -> otherQualityChange(item);
-            };
-            adjustQuality(item, adjustment);
-
-            decreaseSellInDate(item);
-
+            updateQualityOf(item);
         }
     }
 
-    private static void decreaseSellInDate(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+    private static void updateQualityOf(Item item) {
+        int adjustment = switch (item.name) {
+            case "Aged Brie" -> agedBrieQualityChange(item);
+            case "Backstage passes to a TAFKAL80ETC concert" -> backstagePassQualityChange(item);
+            case "Sulfuras, Hand of Ragnaros" -> 0;
+            default -> othersQualityChange(item);
+        };
+        adjustQuality(item, adjustment);
+        decreaseSellInDate(item);
     }
 
-    private static int otherQualityChange(Item item) {
-        return isExpired(item) ? -2 : -1;
-    }
-
-    private static int ageBrieQualityChange(Item item) {
+    private static int agedBrieQualityChange(Item item) {
         return isExpired(item) ? 2 : 1;
-    }
-
-    private static boolean isExpired(Item item) {
-        return item.sellIn < 1;
     }
 
     private static int backstagePassQualityChange(Item item) {
@@ -52,8 +40,22 @@ class GildedRose {
         }
     }
 
+    private static int othersQualityChange(Item item) {
+        return isExpired(item) ? -2 : -1;
+    }
+
+    private static boolean isExpired(Item item) {
+        return item.sellIn < 1;
+    }
+
     private static void adjustQuality(Item item, int adjustment) {
         item.quality = Math.max(0, Math.min(50, item.quality + adjustment));
+    }
+
+    private static void decreaseSellInDate(Item item) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
     }
 
 }
